4bf839a39ca90fe914bb0cb226512f499dd6e89f
Mirec91 <adkgombasky@centrum.sk>
2023-08-06 14:38:59 +0200

    Render photo in View test.

---
+++ PhotoGalleryApp/Controllers/AlbumController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhotoGalleryApp.Data;
+using PhotoGalleryApp.Models;
+
+namespace PhotoGalleryApp.Controllers
+{
+    public class AlbumController : Controller
+    {
+        private readonly DataContext _context;
+
+        public AlbumController(DataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Photo> forestPhotos = _context.Photos.Where(x => x.AlbumCategory == Enums.AlbumNumber.Forest);
+            return View(forestPhotos);
+        }
+    }
+}
