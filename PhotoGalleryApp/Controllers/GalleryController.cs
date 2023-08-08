426ce38d48ecd9493fdc23251635adf9843509db
Mirec91 <adkgombasky@centrum.sk>
2023-08-08 16:22:50 +0200

    Gallery layout + modal test

---
+++ PhotoGalleryApp/Controllers/GalleryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhotoGalleryApp.Data;
+using PhotoGalleryApp.Models;
+
+namespace PhotoGalleryApp.Controllers
+{
+    public class GalleryController : Controller
+    {
+        private readonly DataContext _context;
+
+        public GalleryController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Forest()
+        {
+            IEnumerable<Photo> photos = _context.Photos.Where( p => p.AlbumCategory == Enums.AlbumNumber.Forest);
+            return View(photos);
+        }
+    }
+}
